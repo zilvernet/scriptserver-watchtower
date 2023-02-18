8ed8d1454c0c64eafd31d54f35f4af39b1b96247
zilvernet <75147745+zilvernet@users.noreply.github.com>
2023-02-18 11:57:11 -0800

    Update watchtower-scheduler.sh

--- scripts/watchtower-scheduler.sh
+++ scripts/watchtower-scheduler.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
-docker stop watchtower
-docker rm watchtower
+
+echo "Loading parameters from watchtower.ini:"
+# Load the timezone from the ini file
+timezone=$(awk -F'=' '/^\[Timezone\]/{f=1} f==1&&$1~/^timezone/{print $2;exit}' watchtower.ini)
+echo "    timezone="$timezone
+
+# Load the cron schedule from the ini file
+cron=$(awk -F "=" '/^\[Schedule\]$/{f=1} f==1 && $1=="cron"{gsub(/"/,"",$2); print $2; f=0}' watchtower.ini)
+echo "    cron=$cron"
+
+# Load the Slack configuration from the ini file
+slack_hook_url=$(awk -F'=' '/^\[Slack\]/{f=1} f==1&&$1~/^slack_hook_url/{print $2;exit}' watchtower.ini)
+echo "    slack_hook_url=$slack_hook_url"
+slack_channel=$(awk -F'=' '/^\[Slack\]/{f=1} f==1&&$1~/^slack_channel/{print $2;exit}' watchtower.ini)
+echo "    slack_channel=$slack_channel"
+
+# Get the current server hostname and add the "watchtower-" prefix
+slack_identifier="watchtower-$(hostname)"
+echo "    slack_identifier=$slack_identifier"
+
+# Run the Watchtower Scheduler
+watchtower=watchtower-scheduler
+echo -e "\nStopping: "
+docker stop $watchtower
+echo -e "\nRemoving: "
+docker rm watchtower-scheduler
+echo -e "\nStarting: \n$watchtower"
 docker pull containrrr/watchtower
 docker run -d \
-    --name watchtower-scheduler \
+    --name "$watchtower" \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -e TZ=America/New_York \
-#    -e WATCHTOWER_NOTIFICATIONS=slack \
-#    -e WATCHTOWER_NOTIFICATION_SLACK_HOOK_URL="https://hooks.slack.com/services/{webhookapi}" \
-#    -e WATCHTOWER_NOTIFICATION_SLACK_IDENTIFIER={server-identifier} \
-#    -e WATCHTOWER_NOTIFICATION_SLACK_CHANNEL=#{slackchannel} \
+    -e TZ="$timezone" \
+    -e WATCHTOWER_NOTIFICATIONS=slack \
+    -e WATCHTOWER_NOTIFICATION_SLACK_HOOK_URL="$slack_hook_url" \
+    -e WATCHTOWER_NOTIFICATION_SLACK_IDENTIFIER="$slack_identifier" \
+    -e WATCHTOWER_NOTIFICATION_SLACK_CHANNEL="$slack_channel" \
     containrrr/watchtower \
     --cleanup \
     --label-enable \
-    --schedule "0 0 4 * * *" #it runs daily at 4am
+    --schedule "$cron"
